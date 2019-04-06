cca7ca3c350a85193a7a1f04d9e2ba4e42e50e7c
Leantn_Jang <43318065+janghs0484@users.noreply.github.com>
2019-04-06 17:55:24 +0900

    0.2 ver

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ var app = new Vue({
   el: '#contents',
   data: {
     todos: [
-      { text: 'ㄱ'},
-      { text: 'ㄴ'},
-      { text: 'ㄷ'},
-      { text: 'ㄹ'},
-      { text: 'ㅁ'}
+      { text: '수학 과제'},
+      { text: '영어 과제'},
+      { text: '칫솔 구매'},
+      { text: '귀가 준비'}
     ],
     posts: [
       { text: '[2020/01/04 15:34] 내가간다님께서 "주말 숙제 마치기"목표를 완료하셨습니다.'},
@@ -15,6 +14,14 @@ var app = new Vue({
       { text: '[2020/01/02 20:11] 내가간다님께서 "준비물 구입"목표를 완료하셨습니다.'},
       { text: '[2020/01/02 00:00] 목도리님께서 "1분기 제작"목표를 지키지 못하셨습니다.'},
     ],
-    login: false
-  }
+    login: true
+  },
+   methods: {
+     addNewTodo: function () {
+       this.todos.push({
+         text: this.newTodoText
+       })
+       this.newTodoText = ''
+     }
+   }
 });
